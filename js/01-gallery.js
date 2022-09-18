d94b27cc4ca0be3f4e57d8e199d3110d123b9c3d
MuffMuffin <ymakukha@gmail.com>
2022-09-18 16:18:34 +0300

    Tasks complete
    First draft

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// HTML population:
+const galleryHTML = document.querySelector(".gallery");
+
+const galleryArray = galleryItems.map(({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src=""
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+});
+
+galleryHTML.innerHTML = galleryArray.join("");
+
+// LazyLoad execution:
+const galleryImgRef = document.querySelectorAll(".gallery img");
+
+const onEntry = (observerEntries) => {
+  observerEntries.forEach(({ target, isIntersecting }) => {
+    if (isIntersecting) {
+      let source = target.dataset.source;
+      target.src = source;
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry);
+
+galleryImgRef.forEach((element) => observer.observe(element));
+
+// Lightbox generation:
+let lightboxModal = {};
+
+document.addEventListener("keydown", () => {
+  if (basicLightbox.visible()) {
+    lightboxModal.close();
+  }
+});
+
+galleryHTML.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    lightboxModal = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+    lightboxModal.show();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// HTML population:
+const galleryHTML = document.querySelector(".gallery");
+
+const galleryArray = galleryItems.map(({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="" 
+  alt="${description}" />
+</a>`;
+});
+
+galleryHTML.innerHTML = galleryArray.join("");
+
+// LazyLoad execution:
+const galleryImgRef = document.querySelectorAll(".gallery img");
+
+const onEntry = (observerEntries) => {
+  observerEntries.forEach(({ target, isIntersecting }) => {
+    if (isIntersecting) {
+      let source = target.parentNode.href;
+      target.src = source;
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry);
+
+galleryImgRef.forEach((element) => observer.observe(element));
+
+// Lightbox generation:
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
